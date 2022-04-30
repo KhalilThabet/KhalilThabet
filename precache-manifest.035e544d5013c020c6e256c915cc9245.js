42f0753cb42d5a1c97c02d8e4703fcddd1433d25
KhalilThabet <khalilthabet@outlook.com>
2022-04-30 17:57:29 +0100

    Website

---
+++ precache-manifest.035e544d5013c020c6e256c915cc9245.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd51aa727258ce2c0be7206467a84668",
+    "url": "/khalilThabet/index.html"
+  },
+  {
+    "revision": "55833d7038a1fe3af00e",
+    "url": "/khalilThabet/static/css/main.ebd4856a.chunk.css"
+  },
+  {
+    "revision": "f319cfd5358653641145",
+    "url": "/khalilThabet/static/js/2.ef5fd08d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/khalilThabet/static/js/2.ef5fd08d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55833d7038a1fe3af00e",
+    "url": "/khalilThabet/static/js/main.b699c9cb.chunk.js"
+  },
+  {
+    "revision": "afcee89b4791ed55872d",
+    "url": "/khalilThabet/static/js/runtime-main.fe8f11ad.js"
+  },
+  {
+    "revision": "9cc3522b55f6ecc4936e258882827950",
+    "url": "/khalilThabet/static/media/Belote.9cc3522b.png"
+  },
+  {
+    "revision": "d7b52ac4cb77725efcc1ee75427737e0",
+    "url": "/khalilThabet/static/media/FR.d7b52ac4.jpg"
+  },
+  {
+    "revision": "3bb5e95f6a02ab8821beaf52957905a6",
+    "url": "/khalilThabet/static/media/GAuto.3bb5e95f.gif"
+  },
+  {
+    "revision": "27c9c6b65c06211ae35db5c957bc7a05",
+    "url": "/khalilThabet/static/media/Keras_logo.27c9c6b6.svg"
+  },
+  {
+    "revision": "07db503fe9dbe339b235593078ed095d",
+    "url": "/khalilThabet/static/media/Medical Practice.07db503f.gif"
+  },
+  {
+    "revision": "69d272bed40d6e1762b66c4ca473ffae",
+    "url": "/khalilThabet/static/media/OpenCV.69d272be.svg"
+  },
+  {
+    "revision": "cc1d337d2998ca041e9150559586f0b4",
+    "url": "/khalilThabet/static/media/PyTorch.cc1d337d.svg"
+  },
+  {
+    "revision": "776fd4869226871c601710961f876ac3",
+    "url": "/khalilThabet/static/media/Slide10.776fd486.png"
+  },
+  {
+    "revision": "203ebdc5a623eb6923a021a4a12dc7d1",
+    "url": "/khalilThabet/static/media/Slide4.203ebdc5.png"
+  },
+  {
+    "revision": "6f83f74d3d9e3e2bc737f66c599156fa",
+    "url": "/khalilThabet/static/media/Tensorflow_logo.6f83f74d.svg"
+  },
+  {
+    "revision": "99b98830c91efa221dec7902bc797e1b",
+    "url": "/khalilThabet/static/media/android-icon.99b98830.svg"
+  },
+  {
+    "revision": "ed66113bf3ee4d49bff4f3e8e44e952f",
+    "url": "/khalilThabet/static/media/angular-icon.ed66113b.svg"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/khalilThabet/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/khalilThabet/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "8cc47c68f0d71b364d6e3b793dbb6e55",
+    "url": "/khalilThabet/static/media/cpp.8cc47c68.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/khalilThabet/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/khalilThabet/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/khalilThabet/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "10b0719343408095f5a9b369e0787115",
+    "url": "/khalilThabet/static/media/firebase-icon.10b07193.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/khalilThabet/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/khalilThabet/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "/khalilThabet/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/khalilThabet/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/khalilThabet/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/khalilThabet/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/khalilThabet/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "38f9a4f29ee819486c821b193f592aed",
+    "url": "/khalilThabet/static/media/logo-vercel.38f9a4f2.svg"
+  },
+  {
+    "revision": "92f9cbc7713fcfc56133585d7d2335c9",
+    "url": "/khalilThabet/static/media/logo.92f9cbc7.png"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/khalilThabet/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "a1d1d74601b0fa0c83f3d9ab2205d516",
+    "url": "/khalilThabet/static/media/microsoft_azure-icon.a1d1d746.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/khalilThabet/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "ecb5ec57f305689a6dc44a0828e4b893",
+    "url": "/khalilThabet/static/media/netlify-icon.ecb5ec57.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/khalilThabet/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "0290801be426c565f11fa298b03b9117",
+    "url": "/khalilThabet/static/media/nvidia.0290801b.svg"
+  },
+  {
+    "revision": "04285548ab262ad114ef63b8e59b6e70",
+    "url": "/khalilThabet/static/media/oracle-icon.04285548.svg"
+  },
+  {
+    "revision": "a10d4c5729dc4c62acb88ba06d0911bb",
+    "url": "/khalilThabet/static/media/php-icon.a10d4c57.svg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/khalilThabet/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "5d183d0d353f56ce40c6c7fc97172ead",
+    "url": "/khalilThabet/static/media/qt.5d183d0d.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/khalilThabet/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/khalilThabet/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/khalilThabet/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/khalilThabet/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "d934aa8a8c13cf1a3efc13b0de12e949",
+    "url": "/khalilThabet/static/media/slide1.d934aa8a.jpg"
+  },
+  {
+    "revision": "864c5762ebaa2036417493935e06950f",
+    "url": "/khalilThabet/static/media/slide2.864c5762.jpg"
+  },
+  {
+    "revision": "5d51a1360a4203a7960eceb0d3b7b253",
+    "url": "/khalilThabet/static/media/slide3.5d51a136.jpg"
+  },
+  {
+    "revision": "332417a7f4156fbd6f81615a48286092",
+    "url": "/khalilThabet/static/media/sqlite-icon.332417a7.svg"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "/khalilThabet/static/media/tailwindcss-icon.760a3ee4.svg"
+  },
+  {
+    "revision": "c75a02343f5bcfb30fd150edc4ecde1c",
+    "url": "/khalilThabet/static/media/wireshark-icon.c75a0234.svg"
+  }
+]);
